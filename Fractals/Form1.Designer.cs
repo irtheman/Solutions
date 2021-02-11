de93fd62ec2173dc81605fb84e743e96b526f490
Matthew Hanna <irtheman@gmail.com>
2021-02-11 15:39:10 -0500

    Added Scaling / Zooming and Undo

--- Fractals/Form1.Designer.cs
+++ Fractals/Form1.Designer.cs
@@ -33,6 +33,15 @@ namespace Fractals
             this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuSaveAs = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuView = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSep = new System.Windows.Forms.ToolStripSeparator();
+            this.mnuScale1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuScale2 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuScale3 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuScale4 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuScale5 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuScale6 = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuFullScale = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuUndo = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuRefresh = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuOptions = new System.Windows.Forms.ToolStripMenuItem();
             this.mnuFractal = new System.Windows.Forms.ToolStripComboBox();
@@ -70,10 +79,75 @@ namespace Fractals
             // mnuView
             // 
             this.mnuView.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.mnuRefresh});
+                this.mnuScale1,
+                this.mnuScale2,
+                this.mnuScale3,
+                this.mnuScale4,
+                this.mnuScale5,
+                this.mnuScale6,
+                this.mnuFullScale,
+                this.mnuSep,
+                this.mnuUndo,
+                this.mnuSep,
+                this.mnuRefresh});
             this.mnuView.Name = "mnuView";
             this.mnuView.Size = new System.Drawing.Size(44, 23);
             this.mnuView.Text = "&View";
+            // 
+            // mnuScale1
+            // 
+            this.mnuScale1.Name = "mnuScale1";
+            this.mnuScale1.Size = new System.Drawing.Size(113, 22);
+            this.mnuScale1.Text = "Scale 0.25x";
+            this.mnuScale1.Tag = 0.25;
+            this.mnuScale1.Click += new System.EventHandler(this.mnuScale_Click);
+            // 
+            // mnuScale2
+            // 
+            this.mnuScale2.Name = "mnuScale2";
+            this.mnuScale2.Size = new System.Drawing.Size(113, 22);
+            this.mnuScale2.Text = "Scale 0.5x";
+            this.mnuScale2.Tag = 0.5;
+            this.mnuScale2.Click += new System.EventHandler(this.mnuScale_Click);
+            // 
+            // mnuScale3
+            // 
+            this.mnuScale3.Name = "mnuScale3";
+            this.mnuScale3.Size = new System.Drawing.Size(113, 22);
+            this.mnuScale3.Text = "Scale 0.75x";
+            this.mnuScale3.Tag = 0.75;
+            this.mnuScale3.Click += new System.EventHandler(this.mnuScale_Click);
+            // 
+            // mnuScale4
+            // 
+            this.mnuScale4.Name = "mnuScale4";
+            this.mnuScale4.Size = new System.Drawing.Size(113, 22);
+            this.mnuScale4.Text = "Scale 2x";
+            this.mnuScale4.Tag = 2.0;
+            this.mnuScale4.Click += new System.EventHandler(this.mnuScale_Click);
+            // 
+            // mnuScale5
+            // 
+            this.mnuScale5.Name = "mnuScale5";
+            this.mnuScale5.Size = new System.Drawing.Size(113, 22);
+            this.mnuScale5.Text = "Scale 4x";
+            this.mnuScale5.Tag = 4.0;
+            this.mnuScale5.Click += new System.EventHandler(this.mnuScale_Click);
+            // 
+            // mnuScale6
+            // 
+            this.mnuScale6.Name = "mnuScale6";
+            this.mnuScale6.Size = new System.Drawing.Size(113, 22);
+            this.mnuScale6.Text = "Scale 8x";
+            this.mnuScale6.Tag = 8.0;
+            this.mnuScale6.Click += new System.EventHandler(this.mnuScale_Click);
+            // 
+            // mnuFullScale
+            // 
+            this.mnuFullScale.Name = "mnuFullScale";
+            this.mnuFullScale.Size = new System.Drawing.Size(113, 22);
+            this.mnuFullScale.Text = "&Full Scale";
+            this.mnuFullScale.Click += new System.EventHandler(this.mnuFullScale_Click);
             // 
             // mnuRefresh
             // 
@@ -81,6 +155,14 @@ namespace Fractals
             this.mnuRefresh.Size = new System.Drawing.Size(113, 22);
             this.mnuRefresh.Text = "&Refresh";
             this.mnuRefresh.Click += new System.EventHandler(this.mnuRefresh_Click);
+            // 
+            // mnuUndo
+            // 
+            this.mnuUndo.Name = "mnuUndo";
+            this.mnuUndo.Size = new System.Drawing.Size(113, 22);
+            this.mnuUndo.Text = "&Undo";
+            this.mnuUndo.ShortcutKeys = System.Windows.Forms.Keys.Control | System.Windows.Forms.Keys.Z;
+            this.mnuUndo.Click += new System.EventHandler(this.mnuUndo_Click);
             // 
             // mnuOptions
             // 
@@ -113,6 +195,9 @@ namespace Fractals
             this.Canvas.Size = new System.Drawing.Size(278, 225);
             this.Canvas.TabIndex = 1;
             this.Canvas.TabStop = false;
+            this.Canvas.MouseDown += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseDown);
+            this.Canvas.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseMove);
+            this.Canvas.MouseUp += new System.Windows.Forms.MouseEventHandler(this.Canvas_MouseUp);
             // 
             // Form1
             // 
@@ -140,9 +225,18 @@ namespace Fractals
         private System.Windows.Forms.ToolStripMenuItem mnuView;
         private System.Windows.Forms.ToolStripMenuItem mnuOptions;
         private System.Windows.Forms.ToolStripMenuItem mnuSaveAs;
+        private System.Windows.Forms.ToolStripMenuItem mnuScale1;
+        private System.Windows.Forms.ToolStripMenuItem mnuScale2;
+        private System.Windows.Forms.ToolStripMenuItem mnuScale3;
+        private System.Windows.Forms.ToolStripMenuItem mnuScale4;
+        private System.Windows.Forms.ToolStripMenuItem mnuScale5;
+        private System.Windows.Forms.ToolStripMenuItem mnuScale6;
+        private System.Windows.Forms.ToolStripMenuItem mnuFullScale;
+        private System.Windows.Forms.ToolStripMenuItem mnuUndo;
         private System.Windows.Forms.ToolStripMenuItem mnuRefresh;
-        private System.Windows.Forms.PictureBox Canvas;
         private System.Windows.Forms.ToolStripComboBox mnuFractal;
+        private System.Windows.Forms.ToolStripSeparator mnuSep;
+        private System.Windows.Forms.PictureBox Canvas;
     }
 }
 
--- Fractals/Form1.cs
+++ Fractals/Form1.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing;
 using System.Windows.Forms;
@@ -10,11 +11,19 @@ namespace Fractals
     {
         private Fractal _fractal;
         private Bitmap _bm;
-        private double _xMin, _xMax, _yMin, _yMax;
+        private Stack<double> _xMin, _xMax, _yMin, _yMax;
+        private bool _drawingBox;
+        private int _startX, _startY, _endX, _endY;
+
 
         public Form1()
         {
             InitializeComponent();
+            
+            _xMin = new Stack<double>();
+            _xMax = new Stack<double>();
+            _yMin = new Stack<double>();
+            _yMax = new Stack<double>();
 
             mnuFractal.SelectedIndex = 0;
 
@@ -23,7 +32,7 @@ namespace Fractals
             _fractal = new Mandelbrot();
             _fractal.MaxIterations = MaxIterations;
 
-            mnuRefresh_Click(null, null);
+            mnuFullScale_Click(null, null);
         }
 
         public int MaxIterations { get; set; }
@@ -39,32 +48,32 @@ namespace Fractals
             Application.DoEvents();
 
             double hgt, wid, mid;
-            double want_aspect = (_yMax - _yMin) / (_xMax - _xMin);
+            double want_aspect = (_yMax.Peek() - _yMin.Peek()) / (_xMax.Peek() - _xMin.Peek());
             double Canvas_aspect = Canvas.ClientSize.Height / (double)Canvas.ClientSize.Width;
             if (want_aspect > Canvas_aspect)
             {
-                wid = (_yMax - _yMin) / Canvas_aspect;
-                mid = (_xMin + _xMax) / 2;
-                _xMin = mid - wid / 2;
-                _xMax = mid + wid / 2;
+                wid = (_yMax.Peek() - _yMin.Peek()) / Canvas_aspect;
+                mid = (_xMin.Peek() + _xMax.Peek()) / 2;
+                _xMin.Push(mid - wid / 2);
+                _xMax.Push(mid + wid / 2);
             }
             else
             {
-                hgt = (_xMax - _xMin) * Canvas_aspect;
-                mid = (_yMin + _yMax) / 2;
-                _yMin = mid - hgt / 2;
-                _yMax = mid + hgt / 2;
+                hgt = (_xMax.Peek() - _xMin.Peek()) * Canvas_aspect;
+                mid = (_yMin.Peek() + _yMax.Peek()) / 2;
+                _yMin.Push(mid - hgt / 2);
+                _yMax.Push(mid + hgt / 2);
             }
 
             int width = Canvas.ClientRectangle.Width;
             int hight = Canvas.ClientRectangle.Height;
-            double realDiv = (_xMax - _xMin) / (width - 1);
-            double imaginaryDiv = (_yMax - _yMin) / (hight - 1);
+            double realDiv = (_xMax.Peek() - _xMin.Peek()) / (width - 1);
+            double imaginaryDiv = (_yMax.Peek() - _yMin.Peek()) / (hight - 1);
 
-            double real = _xMin;
+            double real = _xMin.Peek();
             for (int X = 0; X < width; X++)
             {
-                double imaginary = _yMin;
+                double imaginary = _yMin.Peek();
                 for (int Y = 0; Y < hight; Y++)
                 {
                     int iteration = _fractal.Calculate(real, imaginary);
@@ -87,15 +96,48 @@ namespace Fractals
             }
 
             Text = "Mandelbrot (" +
-                _xMin.ToString("0.000000") + ", " +
-                _yMin.ToString("0.000000") + ")-(" +
-                _xMax.ToString("0.000000") + ", " +
-                _yMax.ToString("0.000000") + ")";
+                _xMin.Peek().ToString("0.000000") + ", " +
+                _yMin.Peek().ToString("0.000000") + ")-(" +
+                _xMax.Peek().ToString("0.000000") + ", " +
+                _yMax.Peek().ToString("0.000000") + ")";
+        }
+
+        private void ScaleMap(double scaleFactor)
+        {
+            double size = scaleFactor * (_xMax.Peek() - _xMin.Peek());
+            if (size > 3.2)
+            {
+                mnuFullScale_Click(null, null);
+                return;
+            }
+
+            double mid = (_xMin.Peek() + _xMax.Peek()) / 2;
+            _xMin.Push(mid - size / 2);
+            _xMax.Push(mid + size / 2);
+
+            size = scaleFactor * (_yMax.Peek() - _yMin.Peek());
+            if (size > 2.4)
+            {
+                mnuFullScale_Click(null, null);
+                return;
+            }
+
+            mid = (_yMin.Peek() + _yMax.Peek()) / 2;
+            _yMin.Push(mid - size / 2);
+            _yMax.Push(mid + size / 2);
+
+            this.Cursor = Cursors.WaitCursor;
+            Application.DoEvents();
+
+            DrawFractal();
+
+            this.Cursor = Cursors.Default;
+            Canvas.Cursor = Cursors.Cross;
         }
 
         private void Form1_ResizeEnd(object sender, EventArgs e)
         {
-            mnuRefresh_Click(null, null);
+            mnuFullScale_Click(null, null);
         }
 
         private void mnuFractal_SelectedIndexChanged(object sender, EventArgs e)
@@ -131,17 +173,111 @@ namespace Fractals
             chosen.MaxIterations = MaxIterations;
             _fractal = chosen;
 
-            mnuRefresh_Click(null, null);
+            mnuFullScale_Click(null, null);
+        }
+
+        private void mnuUndo_Click(object sender, EventArgs e)
+        {
+            if (_xMin.Count < 2) return;
+
+            _xMin.Pop();
+            _xMax.Pop();
+            _yMin.Pop();
+            _yMax.Pop();
+
+            DrawFractal();
         }
 
         private void mnuRefresh_Click(object sender, EventArgs e)
         {
-            _xMin = _fractal.MinX;
-            _xMax = _fractal.MaxX;
-            _yMin = _fractal.MinY;
-            _yMax = _fractal.MaxY;
+            ScaleMap(1.0);
+        }
+
+        private void mnuScale_Click(object sender, EventArgs e)
+        {
+            ToolStripMenuItem mnu = sender as ToolStripMenuItem;
+            ScaleMap((double) mnu.Tag);
+        }
+
+        private void mnuFullScale_Click(object sender, EventArgs e)
+        {
+            _xMin.Push(_fractal.MinX);
+            _xMax.Push(_fractal.MaxX);
+            _yMin.Push(_fractal.MinY);
+            _yMax.Push(_fractal.MaxY);
+
+            this.Cursor = Cursors.WaitCursor;
+            Application.DoEvents();
 
             DrawFractal();
+
+            this.Cursor = Cursors.Default;
+            Canvas.Cursor = Cursors.Cross;
+        }
+
+        private void Canvas_MouseDown(object sender, MouseEventArgs e)
+        {
+            _drawingBox = true;
+
+            _startX = e.X;
+            _startY = e.Y;
+            _endX = e.X;
+            _endY = e.Y;
+        }
+
+        private void Canvas_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (!_drawingBox) return;
+
+            _endX = e.X;
+            _endY = e.Y;
+
+            int left = (int)(Math.Min(_startX, _endX));
+            int top = (int)(Math.Min(_startY, _endY));
+            int right = (int)(Math.Abs(_startX - _endX));
+            int bottom = (int)(Math.Abs(_startY - _endY));
+
+            Bitmap bm = new Bitmap(_bm);
+            Graphics gr = Graphics.FromImage(bm);
+            gr.DrawRectangle(Pens.Yellow, left, top, right, bottom);
+            Canvas.Image = bm;
+        }
+
+        private void Canvas_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (!_drawingBox) return;
+
+            _drawingBox = false;
+            Canvas.Image = _bm;
+
+            _endX = e.X;
+            _endY = e.Y;
+
+            // Arrange top left to bottom right
+            double x1 = Math.Min(_startX, _endX);
+            double x2 = Math.Max(_startX, _endX);
+            if (x1 == x2) x2 = x1 + 1;
+
+            double y1 = Math.Min(_startY, _endY);
+            double y2 = Math.Max(_startY, _endY);
+            if (y1 == y2) y2 = y1 + 1;
+
+            // Convert to drawing coordinates
+            double factor = (_xMax.Peek() - _xMin.Peek()) / Canvas.ClientSize.Width;
+            _xMax.Push(_xMin.Peek() + x2 * factor);
+            _xMin.Push(_xMin.Peek() + x1 * factor);
+
+            factor = (_yMax.Peek() - _yMin.Peek()) / Canvas.ClientSize.Height;
+            _yMax.Push(_yMin.Peek() + y2 * factor);
+            _yMin.Push(_yMin.Peek() + y1 * factor);
+
+            this.Cursor = Cursors.WaitCursor;
+            Application.DoEvents();
+
+            DrawFractal();
+
+            this.Cursor = Cursors.Default;
+            Canvas.Cursor = Cursors.Cross;
         }
     }
 }
